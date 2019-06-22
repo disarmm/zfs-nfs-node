cafc5ef1bbcb3f650343fd2178f3f2237313c75f
disarmm <leonardsjunk@gmail.com>
2019-06-22 23:07:24 +0000

    update

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export NFSPATH
 echo $NFSPATH' *(rw,sync,no_subtree_check)' >> /etc/exports
-echo $HOSTNAME'	127.0.0.1' >> /etc/hosts
+echo $HOSTNAME' 127.0.0.1' >> /etc/hosts
 exportfs -a
 service nfs-kernel-server start
