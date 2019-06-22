9794eeb361b2a94f5559f021bfb57bf6b0ac78d5
disarmm <leonardsjunk@gmail.com>
2019-06-22 23:35:01 +0000

    change

--- config.sh
+++ config.sh
@@ -2,5 +2,7 @@
 export NFSPATH
 echo $NFSPATH' *(rw,sync,no_subtree_check)' >> /etc/exports
 echo $HOSTNAME' 127.0.0.1' >> /etc/hosts
+mkdir /run/sendsigs.omit.d/
 exportfs -a
+service rpcbind start
 service nfs-kernel-server start
